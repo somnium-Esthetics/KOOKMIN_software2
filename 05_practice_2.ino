93914c2ec7f200be24a760df33b3700e8cd6ac5f
박우승 <102569953+somnium-Esthetics@users.noreply.github.com>
2023-09-19 10:49:54 +0900

    Update 05_practice_2.ino
    1초간 LED등 점등 후 1초에 5회 점멸 반복 후 LED 소등
    과제코드 05P12

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -8,22 +8,23 @@ void setup() {
   // 1초 동안 LED를 켬
   digitalWrite(ledPin, 0);
   delay(1000);
+}
 
-  // 다음 1 초 동안 LED를 5회 반복 즉 0.2초 주기
+void loop() {
+
+  while (1) {
+    // 다음 1 초 동안 LED를 5회 반복 즉 0.2초 주기
   for (int i = 0; i < 5; i++) {
     digitalWrite(ledPin, 1);
     delay(100);  // LED를 끄고 0.1초 대기
     digitalWrite(ledPin, 0);
     delay(100);  // LED를 켜고 0.1초 대기
   }
+  
 
-  // LED 끔
-  digitalWrite(ledPin, 1);
-}
-
-void loop() {
-  while (1) {
-    // 무한 루프 
-    
+   while (1) {
+   digitalWrite(ledPin, 1 ); //무한루프 
   }
+  }
+  
 }
